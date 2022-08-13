74df30bdd36ab605f16f282c829dcff180562d62
hello <hello>
2022-08-13 15:36:02 +0900

    030복습2

--- Week6/CameraViewController.swift
+++ Week6/CameraViewController.swift
@@ -11,7 +11,17 @@ import Alamofire
 import SwiftyJSON
 import YPImagePicker
 
+/*UIImagePickerController
+ 1. : 사진관리 인스턴스 선언
+ 2. : 프로토콜 연결
+ 3. : 프로토콜 선언
+ 3-1. : 뷰 기능 사용시(didFinishPickingMediaWithInfo: 사진을 선택 또는 카메라 촬영 직후 실행)
+ 3-2. : 취소 버튼 클릭시
+ 4. : 버튼액션을 통한 기능 구현(YPImagePickerButtonClicked, cameraButtonClicked, photoLIbraryButton 등)
+ 
+ */
 //빌드할 때 권한 전부 허용하기
+
 class CameraViewController: UIViewController {
     
     @IBOutlet weak var resultImageView: UIImageView!
@@ -19,13 +29,12 @@ class CameraViewController: UIViewController {
     //UIImagePickerController1. : 사진관리 인스턴스 선언
     let picker = UIImagePickerController()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //UIImagePickerController2. : 프로토콜 연결
         picker.delegate = self
-        
         
     }
     //open source
@@ -48,7 +57,7 @@ class CameraViewController: UIViewController {
         }
         present(picker, animated: true, completion: nil)
     }
-
+    
     
     //UIImagePickerController
     //카메라 사용기기인지 판단(isSourceTypeAvailable(.camera)) *리미티드 에디션처럼 카메라 없는 아이폰도 있다고 함.
@@ -82,15 +91,18 @@ class CameraViewController: UIViewController {
     
     @IBAction func saveToPhotoLibrary(_ sender: UIButton) {
         //resultImageView에 사진이 있으면 저장할 수 있음(UIImageWriteToSavedPhotosAlbum)
-        print(#function)
+        
         if let image = resultImageView.image {
+            print(image)
             UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            
+        }
+        
     }
     
-    }
     
-    /*
     @IBAction func clovaFaceButtonClicked(_ sender: UIButton) {
+        
         let url = "https://openapi.naver.com/v1/vision/celebrity"
         let header: HTTPHeaders = [
             "X-Naver-Client-Id": "tfYqqDDQUPRUW3CIm5x4",
@@ -98,51 +110,47 @@ class CameraViewController: UIViewController {
             "Content-Type": "multipart/form-data"
         ]
         
-        
+        //얼굴인식과정: 이미지뷰->네이버->얼굴분석요청->응답
+        //문자열과 다르게 파일, 이미지, pdf 등은 파일 자체가 그대로 전송되지 않기 때문에 텍스트형태로 인코딩하여 전송해야 한다.(multipartFormData)
         //uiimage를 텍스트 형태로 변환해서 전달
-        //문자열과 다르게 파일, 이미지, pdf 등은 파일 자체가 그대로 전송되지 않기 때문에 텍스트형태로 인코딩하여 전송함.
-        //서버에 전송되는 파일 종류를 명시 : Content-Type(콘텐트 타입은 라이브러리에 포함되어 있기 때문에 생략해도 됨)
+        //서버는 전송되는 내용을 모르기 때문에 전송 파일 종류를 명시해줘야 한다. : Content-Type(콘텐트 타입은 라이브러리에 포함되어 있기 때문에 생략해도 됨)
         guard let imageData = resultImageView.image?.pngData() else { return }
-            AF.upload(multipartFormData: { multipartFormData in
-                multipartFormData.append(Data("one".utf8), withName: "image")
-            }, to: url, headers: header)
-            .validate(statusCode: 200..<400).responseData { response in
-                        
-                        switch response.result {
-                        case .success(let value):
-                            let json = JSON(value)
-                            
-                        case .failure(let error):
-                            print(error)
-                        }
-    }
-    
-    
-}
-}
-*/
-}
-
-
-//UIImagePickerController3. : 프로토콜 선언
-//네비게이션 컨트롤러를 상속받기 때문에 익스텐션에서 위임받아야함.
-extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    //UIImagePickerController4. : 뷰 기능 사용시(didFinishPickingMediaWithInfo: 사진을 선택 또는 카메라 촬영 직후 실행)
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        print(#function)
-        
-        //infokey: 원본, 편집, 메타 데이터 등
-        //편집된 이미지 정보가 UIImage형식이 맞다면 image프로퍼티 값이 된다.
-        //피커의 소스타입에 따라 다른 값을 가져오고 싶다면 조건문 사용 가능(picker.sourceType == .camera {})
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            self.resultImageView.image = image
-            dismiss(animated: true)
+        AF.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(imageData, withName: "image")
+        }, to: url, headers: header)
+        .validate(statusCode: 200..<400).responseData { response in
+            
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+                
+            case .failure(let error):
+                print(error)
+            }
+            
         }
     }
-     
-    //UIImagePickerController5. : 취소 버튼 클릭시
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        print(#function)
-    }
 }
-
+    
+    //UIImagePickerController3. : 프로토콜 선언
+    //네비게이션 컨트롤러를 상속받기 때문에 익스텐션에서 위임받아야함.
+    extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        //UIImagePickerController3-1. : 뷰 기능 사용시(didFinishPickingMediaWithInfo: 사진을 선택 또는 카메라 촬영 직후 실행)
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            print(#function)
+            
+            //infokey: 원본, 편집, 메타 데이터 등
+            //편집된 이미지 정보가 UIImage형식이 맞다면 image프로퍼티 값이 된다.
+            //피커의 소스타입에 따라 다른 값을 가져오고 싶다면 조건문 사용 가능(picker.sourceType == .camera {})
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                self.resultImageView.image = image
+                dismiss(animated: true)
+            }
+        }
+        
+        //UIImagePickerController3-2. : 취소 버튼 클릭시
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            print(#function)
+        }
+    }
